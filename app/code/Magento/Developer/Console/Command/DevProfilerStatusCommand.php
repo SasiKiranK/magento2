8f7cdb2a245b67290eab1649d6b46c738cfd97ed
sasikiran <sasikiran.k@innoppl.com>
2017-05-16 17:25:16 +0530

    Added changes

--- app/code/Magento/Developer/Console/Command/DevProfilerStatusCommand.php
+++ app/code/Magento/Developer/Console/Command/DevProfilerStatusCommand.php
@@ -4,6 +4,9 @@ namespace Magento\Developer\Console\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Magento\Config\Model\Config\Factory as ConfigFactory;
+use Magento\Framework\App\DeploymentConfig\Reader as DeploymentConfigReader;
+use Magento\Framework\Config\File\ConfigFilePool;
 
 use Magento\Framework\Profiler;
 
@@ -11,13 +14,19 @@ class DevProfilerStatusCommand extends Command
 {
 
    private $profiler;
+    protected $configFactory;
+    protected $deploymentConfigReader;
    
    public function __construct(
        Profiler $profiler,
+       ConfigFactory $configFactory,
+       DeploymentConfigReader $deploymentConfigReader,
        $name = null
    ) {
        parent::__construct($name);
        $this->profiler = $profiler;
+       $this->configFactory = $configFactory;
+       $this->deploymentConfigReader = $deploymentConfigReader;
    }
 
   protected function configure()
@@ -27,11 +36,35 @@ class DevProfilerStatusCommand extends Command
   }
   protected function execute(InputInterface $input, OutputInterface $output)
   { 
-      $isEnabled = $this->profiler->isEnabled();      
+
+
+      // $config = $this->configFactory->create();
+      //   $config->setDataByPath('profiler/general/enable', true);
+      //   $config->save();
+         // $this->enableDbProfiler();
+        // return true;
+
+
+     // $this->profiler->start();
+    // \Magento\Framework\Profiler::enable();
+    \Magento\Framework\Profiler::start('TEST: ' . __METHOD__, ['group' => 'TEST', 'method' => __METHOD__]);
+      $isEnabled = \Magento\Framework\Profiler::isEnabled();      
       $status = "Enabled";  
       if (!$isEnabled)
        $status = "Disabled";  
 
       $output->writeln("Profiler Status is : $status");
   }
+
+   public function enableDbProfiler()
+    {
+        $env = $this->deploymentConfigReader->load(ConfigFilePool::APP_ENV);
+        $env['db']['connection']['default']['profiler'] = [
+            'class'   => '\\Magento\\Framework\\DB\\Profiler',
+            'enabled' => true,
+        ];
+        $this->deploymentConfigWriter->saveConfig([ConfigFilePool::APP_ENV => $env], true);
+        return true;
+    }
+
 }
--- index.php
+++ index.php
@@ -18,6 +18,7 @@
  * See COPYING.txt for license details.
  */
 
+#  $_SERVER['MAGE_PROFILER']=1;
 try {
     require __DIR__ . '/app/bootstrap.php';
 } catch (\Exception $e) {
